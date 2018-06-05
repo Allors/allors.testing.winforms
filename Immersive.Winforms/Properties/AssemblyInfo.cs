d861284e0b38ed24486a3cc9253d15fb2f27be54
Koen Van Exem <koen@vanexem.be>
2018-06-05 18:22:34 +0200

    nuget id

--- Immersive.Winforms/Properties/AssemblyInfo.cs
+++ Immersive.Winforms/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Allors.Immersive.Winforms")]
+[assembly: AssemblyTitle("Immersive.Winforms")]
 [assembly: AssemblyDescription("Immersive library for Winforms")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Allors bvba")]
